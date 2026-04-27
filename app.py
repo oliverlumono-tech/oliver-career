1982cf6169b5c34e3d16cc2556314b972c80f138
oliver Lumono <oliverlumon8@gmail.com>
2026-04-27 17:22:23 +0300

    add dynamic data and API route"

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify, send_from_directory
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Kampala, Uganda',
+        'salary': 'UGX 1,000,000'
+    },
+    {
+        'id': 2,
+        'title': 'Software Engineer',
+        'location': 'Jinja, Uganda',
+        
+    },
+    {
+        'id': 3,
+        'title': 'AI Engineer',
+        'location': 'Remote',
+        'salary': '$3,000 per month'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$120,000 per month'
+    },
+]
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'logo.jpg', mimetype='image/jpeg')
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', jobs=JOBS, company_name='Oliver ')
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
